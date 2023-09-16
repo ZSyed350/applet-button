8aa07c37364c76aac26487810bd284cf2d69a52f
Ernest Wong <ernestwong1027@gmail.com>
2023-09-16 13:38:05 -0400

    made ui

---
+++ applet-button/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';  // Import the styles
+import Terminal from './Terminal';
+
+function App() {
+  return (
+    <div className="App">
+      <Terminal />
+    </div>
+  );
+}
+
+export default App;
