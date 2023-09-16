e368aeefe55d201c92baa37e73f0d4a969f98ac7
Victor Wei <victoryqwei@gmail.com>
2023-09-16 15:36:43 -0400

    fix: center nav bar

--- applet-button/src/Grid.js
+++ applet-button/src/Grid.js
@@ -1,41 +1,41 @@
 // Grid.js
-import React from 'react';
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
 function Grid({ numButtons }) {
-    const buttons = [];
+  const buttons = [];
 
-    // const fs = require('fs');
-    // const directoryPath = '../../applets';
+  // const fs = require('fs');
+  // const directoryPath = '../../applets';
 
-    // fs.readdir(directoryPath, (err, files => {
-    //     if (err) {
-    //         console.error('Error reading directory: ', err);
-    //         return;
-    //     }
+  // fs.readdir(directoryPath, (err, files => {
+  //     if (err) {
+  //         console.error('Error reading directory: ', err);
+  //         return;
+  //     }
 
-    //     console.log('Files in the directory', files);
+  //     console.log('Files in the directory', files);
 
-    //     // process the files
-    // }));
-    // parse the files
-    // get the names
-    // numButtons = numFiles
+  //     // process the files
+  // }));
+  // parse the files
+  // get the names
+  // numButtons = numFiles
 
-    for (let i=1; i <= numButtons; i++) {
-        buttons.push(
-            <Button
-                key={i}
-                label={'Button $[i]'}
-                onClick={() => {
-                    // Handle button click here
-                    console.log('Button ${i} clicked');
-                }}
-            />
-        );
-    }
+  for (let i = 1; i <= numButtons; i++) {
+    buttons.push(
+      <Button
+        key={i}
+        label={"Button $[i]"}
+        onClick={() => {
+          // Handle button click here
+          console.log("Button ${i} clicked");
+        }}
+      />
+    );
+  }
 
-    return <div className="button-grid">{buttons}</div>;
+  return <div className="button-grid">{buttons}</div>;
 }
 
 export default Grid;
