ed84fd455804e1fa19c16743554d2eda65afb73a
zara <zsyed350@gmail.com>
2023-09-16 15:25:50 -0400

    put grid in myapplets

--- applet-button/src/MyApplets.js
+++ applet-button/src/MyApplets.js
@@ -1,10 +1,12 @@
 // Terminal.js
 import { Box, Button, Text } from "@chakra-ui/react";
 import React from "react";
+import Grid from "./Grid";
 
 function MyApplets() {
   return (
     <Box>
+      <Grid />
       <Text>adsfiuosafhioasdhfiahf</Text>
       <Button>Test</Button>
     </Box>
